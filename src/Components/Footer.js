b978fbc908fd254263d54a7ba9f2d5970d0d17df
jeremy-jefferson <jeremyejefferson@gmail.com>
2023-11-28 01:06:33 -0600

    updates to the footer and css

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,12 +23,13 @@ class Footer extends Component {
               <ul className="social-links">{networks}</ul>
 
               <ul className="copyright">
-                <li>&copy; Copyright 2023 Nordic Giant</li>
+                <li>CRAFTED BY</li>
                 <li>
-                  Design by{" "}
+                {" "}
                   <a title="Styleshout" href="http://www.styleshout.com/">
-                    Styleshout
+                    Jeremy E. Jefferson 
                   </a>
+                   &copy; 2023
                 </li>
               </ul>
             </div>
